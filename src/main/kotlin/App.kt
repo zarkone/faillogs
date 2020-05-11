31821eb85deaf865149b5b9283d6988ca654f6a1
Anatolii Smolianinov <zarkonesmall@gmail.com>
2020-05-11 19:10:53 +0200

    add usage; introduce `cmd`

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,19 @@
 @file:JvmName("App")
 package org.zarkone.faillogs
 
+fun printHelp() {
+    System.err.println("Usage:")
+    System.err.println("faillogs username/repo")
+}
 fun main(args: Array<String>) {
-    val repo = args[0]
+    if (args.isEmpty() || args[0] == "--help") {
+        printHelp()
+        return
+    }
+
+    val cmd = args[0]
+
+    val repo = cmd
     val conf = Config()
     val githubRequest = GithubRequest(conf, repo)
 
